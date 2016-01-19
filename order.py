95fd8b3af3b0bd2885d8c74ca8192535b1ddea87
Daniel Koics <koics.dani@gmail.com>
2016-01-19 15:48:33 +0100

    Sceleton of classes created.

---
+++ order.py
@@ -0,0 +1,17 @@
+__author__ = 'KoicsD'
+
+
+class Order:
+    @classmethod
+    def parse(cls, csv_row: str):
+        pass
+
+    def persist(self, cursor_obj):
+        pass
+
+    def to_csv(self):
+        pass
+
+    @classmethod
+    def select(cls):
+        pass
