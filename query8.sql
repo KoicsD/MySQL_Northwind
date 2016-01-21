d07c6a2b0274630437d1103d54cfd59fc237c35d
Daniel Koics <koics.dani@gmail.com>
2016-01-21 16:10:13 +0100

    query8 done and accepted

--- query8.sql
+++ query8.sql
@@ -1,16 +1,25 @@
 SELECT
-	*
-FROM
 	(
 		SELECT
-			CategoryName,
-			ProductName,
-			UnitPrice
-		FROM
-			Categories
-		NATURAL JOIN
-			Products
-		ORDER BY
 			CategoryName
-	)
-		AS Joint;
+        FROM
+			Categories
+		WHERE
+			Categories.CategoryID = o.CategoryID
+    )
+		AS CategoryName,
+    ProductName AS CheapestProduct,
+    UnitPrice AS MinCategoryPrice
+FROM
+	Products as o
+WHERE
+	UnitPrice = (
+					SELECT
+						MIN(UnitPrice)
+					FROM
+						Products AS i
+					WHERE
+						i.CategoryID = o.CategoryID
+                )
+ORDER BY
+	CategoryName;
