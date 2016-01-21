f560a11a1f71a0e3057574358ffbf641b243bc4b
Daniel Koics <koics.dani@gmail.com>
2016-01-21 19:41:24 +0100

    query3 written but not accepted

--- query3.sql
+++ query3.sql
@@ -3,37 +3,64 @@ SELECT
 	ShipName,
 	ShipAddress,
 	ShipCity,
-	ShipRegion, 
+	ShipRegion,
 	ShipPostalCode,
 	ShipCountry,
 	Orders.CustomerID AS CustomerID,
     -- from Customers:
-	Customers.CompanyName AS CustomerName, -- the name of the customer's company
+	CompanyName AS CustomerName, -- the name of the customer's company
 	Address,
 	City,
 	Region,
 	PostalCode,
 	Country,
-    -- exception:
-	-- Salesperson, -- the fullname of the employee separated by space
-    -- from Customers again
+    -- from Orders:
+	(
+		SELECT
+			CONCAT(FirstName, ' ', LastName)
+		FROM
+			Employees
+		WHERE
+			Employees.EmployeeID = Orders.EmployeeID
+    )
+		AS Salesperson, -- the fullname of the employee separated by space (from Employees)
 	Orders.OrderID AS OrderID,
 	OrderDate,
 	RequiredDate,
 	ShippedDate,
-    -- from Shippers:
-	-- ShipperName,
+	(
+		SELECT
+			CompanyName
+		FROM
+			Shippers
+		WHERE
+			Shippers.ShipperID = Orders.ShipVia
+    )
+		AS ShipperName, -- from Shippers
     -- from OrderDetails:
 	ProductID,
-	-- ProductName, -- exception
+    (
+		SELECT
+			ProductName
+		FROM
+			Products
+		WHERE
+			Products.ProductID = OrderDetails.ProductID
+    )
+		AS ProductName, -- from Products
 	UnitPrice,
 	Quantity,
-	Discount
-	-- Total -- see previous query (more detailed orders)
-	-- WTF?:
-    -- Freight
+	Discount,
+	(
+		UnitPrice * Quantity * (1 - Discount)
+    )
+		AS Total, -- see previous query (more detailed orders)
+    -- from Orders:
+    Freight
 FROM Customers
 	JOIN Orders
 		ON Customers.CustomerID = Orders.CustomerID
 	JOIN OrderDetails
-		ON Orders.OrderID = OrderDetails.OrderID;
+		ON Orders.OrderID = OrderDetails.OrderID
+ORDER BY
+	Customers.CustomerID;
