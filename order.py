86ded1a6cd5fc8bcf95a0347c8f6a915479d0389
Daniel Koics <koics.dani@gmail.com>
2016-01-23 19:19:47 +0100

    bugfix: Freight is type of decimal, which translates to float (instead of int) in python

--- order.py
+++ order.py
@@ -5,6 +5,6 @@ from record_template import *
 class Order(Record):
     table_name = "Orders"
     fields = (("OrderID", int), ("CustomerID", str), ("EmployeeID", int), ("OrderDate", datetime),
-              ("RequiredDate", datetime), ("ShippedDate", datetime), ("ShipVia", int), ("Freight", int),
+              ("RequiredDate", datetime), ("ShippedDate", datetime), ("ShipVia", int), ("Freight", float),
               ("ShipName", str), ("ShipAddress", str), ("ShipCity", str), ("ShipRegion", str), ("ShipPostalCode", str),
               ("ShipCountry", str))
